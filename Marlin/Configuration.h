2803d948b1ecd869496820dc3f91a054daef64c2
Denny Lia <liaden6549@gmail.com>
2025-01-10 20:09:55 -0500

    Configuration.h changes

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1838,7 +1838,8 @@
  *     O-- FRONT --+
  */
 #if ENABLED(KNUTWURST_BLTOUCH)
-  #define NOZZLE_TO_PROBE_OFFSET { -2, -25, -0.4 } // https://www.thingiverse.com/thing:2824005
+  #define NOZZLE_TO_PROBE_OFFSET { 10, -54, 0 } //dsl 4MAXP2, ZOffset will be calculated using Special Menu Auto Level Probe and Nozzle Dn
+  //dsl #define NOZZLE_TO_PROBE_OFFSET { -2, -25, -0.4 } // https://www.thingiverse.com/thing:2824005
   // #define NOZZLE_TO_PROBE_OFFSET { 29, -15, 0 } //X-Carriage
 #endif
 
