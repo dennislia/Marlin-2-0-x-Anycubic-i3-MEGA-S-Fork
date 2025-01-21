9bd6dff21a38461fdf547a9b39f716fb953f0719
Denny Lia <liaden6549@gmail.com>
2025-01-21 12:04:23 -0500

    fix typo in last commit

--- Marlin/src/lcd/extui/knutwurst/anycubic_touchscreen.cpp
+++ Marlin/src/lcd/extui/knutwurst/anycubic_touchscreen.cpp
@@ -551,7 +551,7 @@ void AnycubicTouchscreenClass::HandleSpecialMenu() {
            (strcasestr_P(currentTouchscreenSelection, PSTR(SM_TEST_NOZ_DN_S)) != NULL)) { //dsl
     SERIAL_ECHOLNPGM("Special Menu: Test Menu: Nozzle Down"); //dsl
 
-    injectCommands(F("M211 S0\nG91\nG1 F400 Z-0.02\nG90\nM211 S"));  //dsl disable endstops, Relative mode, Move -0.02mm, Absolute mode, enable endstops
+    injectCommands(F("M211 S0\nG91\nG1 F400 Z-0.02\nG90\nM211 S1"));  //dsl disable endstops, Relative mode, Move -0.02mm, Absolute mode, enable endstops
     setZOffset_mm(getZOffset_mm() - 0.02F);    //dsl Update the zoffset variable and display   
     
      
